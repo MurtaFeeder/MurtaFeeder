cd99c2a63ec3f5cace1ce0acbea08d30ee2144e6
Andreu Sureda <suredaps4@gmail.com>
2023-05-19 15:57:51 +0200

    New implementation :
    * Handle button
    * Dispense as independent Task
    * Debug prints

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,158 @@
 #include <WiFiManager.h>
 #include <PubSubClient.h>
-#include <time.h>
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
+#include <ezButton.h>
 #include "envs.h"
-#include "dimensions.h"
-#include "actions.h"
 
+#define FEEDER_HEIGHT 20
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
+#define TRIG_PIN 13
+#define ECHO_PIN 12
+#define BUTTON_PIN 33
+#define BUTTON_DEBOUNCE 50
+#define MIN_DISPENSE_PRESS_TIME 1500
+#define MAX_DISPENSE_PRESS_TIME 4000
+#define MAX_CONFIRM_DISPENSE_TIME 10000
+#define LONG_PRESS_TIME 7000
+#define SERVO_PIN 27
+#define SERVO_STOP 90
+#define SERVO_SPEED 5
 
-const int TRIG_PIN = 13;
-const int ECHO_PIN = 12;
+#define MAX_JSON_LENGTH 200
 
-const int SERVO_PIN = 27;
-const int SERVO_STOP = 90;
-int speed = 5;
-
-unsigned long getTime()
-{
-  time_t now;
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo))
-  {
-    // Serial.println("Failed to obtain time");
-    return (0);
-  }
-  time(&now);
-  return now;
-}
-
-Servo myServo;
-
-String getFeederName()
+String getFeederName(bool lowerCase)
 {
   String apName = "MurtaFeeder_";
   String chipId = String(WIFI_getChipId(), HEX);
   chipId.toUpperCase();
   apName += chipId;
 
+  if (lowerCase)
+  {
+    apName.toLowerCase();
+  }
+
   return apName + "";
 }
 
-const String feederName = getFeederName();
+const String feederName = getFeederName(false);
+const String lowerFeederName = getFeederName(true);
+
+/* WIFI VARS */
+WiFiManager wm;
+/* -------- */
+
+/* STATUS VARS */
+unsigned long lastStatusTime = 0;
+/* --------- */
+
+/* BUTTON  VARS*/
+ezButton button(BUTTON_PIN);
+unsigned long pressedTime = 0;
+unsigned long releasedTime = 0;
+unsigned long lastCountTime = 0;
+bool countStarted = false;
+/* ----- */
+
+/* MQTT VARS */
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+String feederSubTopic;
+String feederPubActionDoneTopic;
+String feederPubStatusTopic;
+/* ------- */
+
+/* DISPENSE TASK VARS */
+Servo myServo;
+QueueHandle_t dispenseQueue;
+struct DispenseParams
+{
+  int portions;
+  char message[MAX_JSON_LENGTH];
+};
+/* --------- */
 
 void setupFeederPins()
 {
   Serial.begin(115200);
-  pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(ECHO_PIN, INPUT);  // Sets the echoPin as an Input
+  /* PROXIMITY SENSOR */
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  /* ----- */
+  /* SWTICH PUSH BUTTON */
+  button.setDebounceTime(BUTTON_DEBOUNCE);
+  button.setCountMode(COUNT_RISING);
+  /* ----- */
+  /* SERVO */
   myServo.attach(SERVO_PIN);
+  /* ----- */
 }
 
 void initWifiManager()
 {
-  WiFiManager wm;
-  // wm.resetSettings();
   bool res;
 
   res = wm.autoConnect(feederName.c_str(), "MurtaFeeder1234");
 
   if (!res)
   {
+#ifdef DEBUG
     Serial.println("Failed to connect");
+#endif
     ESP.restart();
   }
   else
   {
-    // if you get here you have connected to the WiFi
+#ifdef DEBUG
     Serial.println("WiFi Connected");
+#endif
+    // if you get here you have connected to the WiFi
   }
 }
 
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-String feederTopic;
-
-void dispense(int portions, StaticJsonDocument<64> event)
+void dispenseTask(void *parameter)
 {
+  DispenseParams *params;
 
-  while (portions > 0)
+  while (true)
   {
-    myServo.write(0);
-    delay(660);
-    myServo.write(SERVO_STOP);
-    portions--;
-    delay(250);
+    xQueueReceive(dispenseQueue, &(params), portMAX_DELAY);
+    int portions = params->portions;
+
+    while (portions > 0)
+    {
+      myServo.write(0);
+      delay(700);
+      myServo.write(SERVO_STOP);
+      portions--;
+      delay(250);
+    }
+
+    char doneMessage[MAX_JSON_LENGTH];
+
+    strcpy(doneMessage, params->message);
+
+    mqttClient.publish(feederPubActionDoneTopic.c_str(), doneMessage);
   }
-
-  String topic = feederTopic + "/done";
-
-  String message;
-  serializeJson(event, message);
-
-  mqttClient.publish(topic.c_str(), message.c_str());
+  vTaskDelete(NULL);
 }
 
 void mqttConsumer(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  String message(payload, length);
   // NO TOPIC CHECK (at the moment), ONLY SUSCRIBED TO 1 TOPIC
-  StaticJsonDocument<64> doc;
+  StaticJsonDocument<96> doc;
 
   DeserializationError error = deserializeJson(doc, payload, length);
 
   if (error)
   {
+#ifdef DEBUG
     Serial.print("deserializeJson() failed: ");
     Serial.println(error.c_str());
+#endif
     return;
   }
 
@@ -117,71 +160,81 @@ void mqttConsumer(char *topic, byte *payload, unsigned int length)
 
   switch (actionID)
   {
-  case DISPENSE:
+  case 1:
     int portions = doc["portions"];
-    dispense(portions, doc);
+
+    DispenseParams *dispenseParams = new DispenseParams;
+    dispenseParams->portions = portions;
+    strcpy(dispenseParams->message, message.c_str());
+    xQueueSend(dispenseQueue, &dispenseParams, portMAX_DELAY);
   }
 }
 
 void connectMQTT()
 {
-  int attempts = 5;
-  while (attempts > 0 && !mqttClient.connected())
+#ifdef DEBUG
+  Serial.print("[MQTT] Connecting to MQTT broker...");
+#endif
+  while (!mqttClient.connected())
   {
-    if (!mqttClient.connect(feederName.c_str(), MQTT_USER, MQTT_PSWD))
+    if (mqttClient.connect(feederName.c_str(), MQTT_USER, MQTT_PSWD))
     {
-      attempts--;
+#ifdef DEBUG
+      Serial.println("\n[MQTT] Connected to MQTT broker!");
+#endif
+      mqttClient.subscribe(feederSubTopic.c_str());
+    }
+    else
+    {
+#ifdef DEBUG
+      Serial.print("[MQTT] Connecting to MQTT broker...");
+#endif
       delay(5000);
     }
   }
+}
 
-  if (attempts == 0)
-  {
-    ESP.restart();
-  }
+void loadMQTTTopics()
+{
+  String baseTopic = "murta/feeder/" + lowerFeederName;
 
-  Serial.println("MQTT Connected");
-
-  feederTopic = "murta/feeder/" + feederName;
-
-  String topic = feederTopic + "/action";
-
-  Serial.print("Suscribed to ");
-  Serial.print(topic);
-
-  boolean res = mqttClient.subscribe(topic.c_str());
-  Serial.print(" (Sub Status:");
-  Serial.print(res);
-  Serial.println(")");
-
-  mqttClient.setCallback(mqttConsumer);
+  feederSubTopic = baseTopic + "/action";
+  feederPubActionDoneTopic = baseTopic + "/done";
+  feederPubStatusTopic = baseTopic + "/status";
+#ifdef DEBUG
+  Serial.printf("[MQTT] Topics Loaded:\n*Sub Topic:%s\n*Pub Action Done Topic:%s\n*Pub Status Topic:%s\n", feederSubTopic.c_str(), feederPubActionDoneTopic.c_str(), feederPubStatusTopic.c_str());
+#endif
 }
 
 void initMQTT()
 {
-
+  loadMQTTTopics();
+  mqttClient.setCallback(mqttConsumer);
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
   connectMQTT();
 }
 
 void setup()
 {
-  delay(10000);
   setupFeederPins();
   initWifiManager();
-  configTime(0, 0, "pool.ntp.org");
   initMQTT();
-}
 
-bool status_flag = true;
+  dispenseQueue = xQueueCreate(10, sizeof(sizeof(DispenseParams *)));
+
+  xTaskCreate(dispenseTask, "DispenseTask", 2048, NULL, 1, NULL);
+}
 
 void getFeederStatus()
 {
-  if (!status_flag)
+  unsigned long timeDiff = millis() - lastStatusTime;
+
+  if (timeDiff < 4900)
   {
-    status_flag = true;
     return;
   }
+
+  lastStatusTime = millis();
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -200,33 +253,104 @@ void getFeederStatus()
     distanceCm = 0;
   }
 
-  float capacity = (FEEDER_STORE_HEIGHT - distanceCm) / 20;
+  float capacity = (FEEDER_HEIGHT - distanceCm) / 20;
 
-  StaticJsonDocument<32> doc;
+  if (capacity > 1)
+  {
+    capacity = 1;
+  }
+  else if (capacity < 0)
+  {
+    capacity = 0;
+  }
 
-  doc["timestamp"] = getTime();
+  StaticJsonDocument<16> doc;
+
   doc["capacity"] = capacity;
 
   String json;
   serializeJson(doc, json);
 
-  String topic = feederTopic + "/status";
+  mqttClient.publish(feederPubStatusTopic.c_str(), json.c_str());
+}
 
-  Serial.print("Feeder Status sent to ");
-  Serial.println(topic);
-  mqttClient.publish(topic.c_str(), json.c_str());
+void resetConfirmState()
+{
+  button.resetCount();
+  lastCountTime = 0;
+  countStarted = false;
+}
 
-  status_flag = false;
+void handleButtonPress()
+{
+  unsigned long confirmDuration = millis() - lastCountTime;
+
+  if (countStarted && confirmDuration > MAX_CONFIRM_DISPENSE_TIME)
+  {
+#ifdef DEBUG
+    Serial.println("[BUTTON HANDLER] Too much to confirm. Reset counter.");
+#endif
+    resetConfirmState();
+  }
+
+  button.loop();
+  if (button.isPressed())
+  {
+    pressedTime = millis();
+  }
+  if (button.isReleased())
+  {
+    releasedTime = millis();
+
+    unsigned long pressDuration = releasedTime - pressedTime;
+
+    /* DISPENSE PRESS */
+    if (MIN_DISPENSE_PRESS_TIME <= pressDuration && pressDuration <= MAX_DISPENSE_PRESS_TIME && confirmDuration <= MAX_CONFIRM_DISPENSE_TIME)
+    {
+      int portions = (int)(button.getCount() - 1);
+      if (portions != 0)
+      {
+#ifdef DEBUG
+        Serial.printf("[BUTTON HANDLER] Dispense confirmed. Dispensing %d portions.\n", portions);
+#endif
+        DispenseParams *dispenseParams = new DispenseParams;
+        dispenseParams->portions = portions;
+        String message = "{\"actionId\":1,\"portions\":" + String(portions) + ",\"feederId\":\"" + lowerFeederName + "\",\"origin\":\"MANUAL\"}";
+        strcpy(dispenseParams->message, message.c_str());
+        xQueueSend(dispenseQueue, &dispenseParams, portMAX_DELAY);
+      }
+      resetConfirmState();
+    }
+    /* RESET PRESS */
+    else if (pressDuration >= LONG_PRESS_TIME)
+    {
+      // if long press remove wifi credentials & restart
+      wm.resetSettings();
+      ESP.restart();
+    }
+    /* COUNT PRESS */
+    else
+    {
+      lastCountTime = releasedTime;
+      countStarted = true;
+
+#ifdef DEBUG
+      Serial.printf("[BUTTON HANDLER] Current counter %lu\n", button.getCount());
+#endif
+    }
+  }
 }
 
 void loop()
 {
   if (!mqttClient.connected())
   {
+#ifdef DEBUG
     Serial.println("MQTT Disconnected : Trying connection");
+#endif
     connectMQTT();
   }
   mqttClient.loop();
   getFeederStatus();
-  delay(2000);
+  handleButtonPress();
 }
